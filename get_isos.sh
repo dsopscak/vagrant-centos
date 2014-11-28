511ffc8811b5620e95d2e4aee63f8bba622e13ec
Skip Sopscak <dsopscak@cox.net>
2014-11-27 22:45:56 -0600

    Update to centos 6.6 and provide hint script for picking up req'd iso's.

---
+++ get_isos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget http://download.virtualbox.org/virtualbox/4.3.14/VBoxGuestAdditions_4.3.14.iso
+wget http://cosmos.cites.illinois.edu/pub/centos/6.6/isos/x86_64/CentOS-6.6-x86_64-minimal.iso
