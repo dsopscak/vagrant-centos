d8a5ddaac2ade064898db67a22eeb315ee751006
Skip Sopscak <dsopscak@cox.net>
2014-11-27 22:46:39 -0600

    Really update to 6.6 and improve get_isos script.

--- get_isos.sh
+++ get_isos.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+if [[ ! -d isos ]]; then mkdir isos; fi
+cd isos
 wget http://download.virtualbox.org/virtualbox/4.3.14/VBoxGuestAdditions_4.3.14.iso
 wget http://cosmos.cites.illinois.edu/pub/centos/6.6/isos/x86_64/CentOS-6.6-x86_64-minimal.iso
